5dad10c85a342495e7e78ae9af82aeed9618c436
Adesegun Adedeji <adesegunadedeji@gmail.com>
2020-06-03 18:22:30 -0700

    Initial Commit

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+
+const URL = `http://api.openweathermap.org/data/2.5/weather`;
+
+const API = `${process.env.REACT_APP_WEATHER}`;
+
+export const fetchWeather = async(query) => {
+    const {data} = await axios.get(URL,{
+        params: {
+            q: query,
+            units:'metric',
+            APPID: API,
+        }
+    });
+    return data;
+}
